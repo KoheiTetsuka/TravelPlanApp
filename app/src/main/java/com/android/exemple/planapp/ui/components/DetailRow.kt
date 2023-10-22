4d2113c36788951ee37d0b31416f16d504d12d91
tetsukak <58130056+KoheiTetsuka@users.noreply.github.com>
2023-10-22 13:31:10 +0900

    詳細画面List初期表示

--- app/src/main/java/com/android/exemple/planapp/ui/components/DetailRow.kt
+++ app/src/main/java/com/android/exemple/planapp/ui/components/DetailRow.kt
@@ -1,11 +1,14 @@
 package com.android.exemple.planapp.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -15,6 +18,7 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,32 +44,37 @@ fun DetailRow(
         Row(
             modifier = Modifier
                 .clickable { navController.navigate("detail/${detail.planId}") }
-                .padding(10.dp),
+                .padding(5.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = if (detail.date == null) "未定" else dateFormat.format(detail.date),
-                fontSize = 15.sp
-            )
+            Column {
+                Text(
+                    modifier = Modifier.background(Color.Yellow),
+                    text = if (detail.date == null) "未定" else dateFormat.format(detail.date),
+                    fontSize = 12.sp,
+                )
+                Spacer(modifier = Modifier.height(3.dp))
+                Text(
+                    text = detail.startTime.toString(),
+                    fontSize = 11.sp
+                )
+                Text(
+                    text = "〜",
+                    fontSize = 11.sp
+                )
+                Text(
+                    text = detail.endTime.toString(),
+                    fontSize = 11.sp
+                )
+            }
+            if (detail.date == null) {
+                Spacer(modifier = Modifier.width(22.dp))
+            }
             Column {
                 Text(
                     fontSize = 20.sp,
                     text = detail.title
                 )
-                Row {
-                    Text(
-                        text = detail.startTime.toString(),
-                        fontSize = 11.sp
-                    )
-                    Text(
-                        text = "〜",
-                        fontSize = 11.sp
-                    )
-                    Text(
-                        text = detail.endTime.toString(),
-                        fontSize = 11.sp
-                    )
-                }
             }
             Spacer(modifier = Modifier.weight(1f))
             IconButton(
