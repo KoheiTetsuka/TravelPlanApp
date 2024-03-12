a4253018c1956009856bb9c72914a3c914ef01c9
tetsukak <58130056+KoheiTetsuka@users.noreply.github.com>
2024-03-12 20:43:37 +0900

    BottomBar strings.xml作成

--- app/src/main/java/com/android/exemple/planapp/ui/components/BottomBar.kt
+++ app/src/main/java/com/android/exemple/planapp/ui/components/BottomBar.kt
@@ -8,32 +8,30 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
-
-sealed class Item(var dist: String, var displayName: String, var icon: ImageVector) {
-    object Plan : Item("Plan", "プラン", Icons.Filled.Edit)
-    object Property : Item("Property", "持ち物", Icons.Filled.List)
-}
+import com.android.exemple.planapp.R
 
 @Composable
 fun BottomBar(navController: NavController, planId: Int) {
-    val items = listOf(Item.Plan, Item.Property)
 
     BottomNavigation {
-        items.forEachIndexed { _, item ->
-            BottomNavigationItem(
-                icon = { Icon(item.icon, contentDescription = item.dist) },
-                label = { Text(item.displayName) },
-                selected = true,
-                onClick = {
-                    if (item.dist == "Plan") {
-                        navController.navigate("detail/${planId}")
-                    } else if (item.dist == "Property") {
-                        navController.navigate("property/${planId}")
-                    }
-                }
-            )
-        }
+        BottomNavigationItem(
+            icon = { Icon(Icons.Filled.Edit, contentDescription = stringResource(R.string.desc_schedule)) },
+            label = { Text( text = stringResource(R.string.label_schedule)) },
+            selected = true,
+            onClick = {
+                    navController.navigate("detail/${planId}")
+            }
+        )
+
+        BottomNavigationItem(
+            icon = { Icon(Icons.Filled.List, contentDescription = stringResource(R.string.desc_property)) },
+            label = { Text( text = stringResource(R.string.label_property)) },
+            selected = true,
+            onClick = {
+                    navController.navigate("property/${planId}")
+            }
+        )
     }
 }
